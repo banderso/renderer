7c85961b85ec9d73291efc19d7e8c43aad2c694c
Benjamin Anderson <me@bander.so>
2014-07-05 18:40:32 -0700

    Added first pass at the mesh system.

---
+++ Renderer/MeshSystem.cpp
@@ -0,0 +1,141 @@
+//
+//  MeshSystem.cpp
+//  Renderer
+//
+//  Created by Benjamin Anderson on 7/5/14.
+//  Copyright (c) 2014 Benjamin Anderson. All rights reserved.
+//
+
+#include "MeshSystem.cpp"
+
+bar::MeshSystem::MeshSystem() {
+  vao_buffers_.vertices  = nullptr;
+  vao_buffers_.normals   = nullptr;
+  vao_buffers_.texcoords = nullptr;
+  vao_buffers_.elements  = nullptr;
+  vao_buffers_.element_types = nullptr;
+  vao_buffers_.element_counts = nullptr;
+}
+
+bar::MeshSystem::MeshSystem(uint32_t max_mesh_count) {
+  vao_buffers_.vertices  = new GLuint[max_mesh_count];
+  vao_buffers_.normals   = new GLuint[max_mesh_count];
+  vao_buffers_.texcoords = new GLuint[max_mesh_count];
+  vao_buffers_.elements  = new GLuint[max_mesh_count];
+  vao_buffers_.element_types = new GLenum[max_mesh_count];
+  vao_buffers_.element_counts = new GLsizei[max_mesh_count];
+}
+
+bar::MeshSystem::~MeshSystem() {
+  delete[] vao_buffers_.vertices;
+  delete[] vao_buffers_.normals;
+  delete[] vao_buffers_.texcoords;
+  delete[] vao_buffers_.elements;
+  delete[] vao_buffers_.element_types;
+  delete[] vao_buffers_.element_counts;
+}
+
+void bar::MeshSystem::load_buffers(GLuint vao_name,
+                                   uint32_t attribute_count,
+                                   const MeshAttribute *attributes) {
+  if (vao_name == 0 ||
+      attribute_count == 0 ||
+      attribute_count > BufferType::COUNT ||
+      attributes == nullptr) {
+    return;
+  }
+
+  glBindVertexArray(vao_name);
+
+  GLenum element_type;
+  GLsizei element_count;
+  GLuint buffers[BufferType::COUNT];
+  glGenBuffers(BufferType::COUNT, buffers);
+  
+  for (uint8_t i = 0; i < attribute_count; i++) {
+    const MeshAttribute *attribute = attributes[i];
+    const BufferType type = attribute->type;
+    const GLSizei type_size = GetGLTypeSize(attribute->data_type);
+    
+    switch (type) {
+      case BufferType::VERTEX:
+      case BufferType::NORMAL:
+      case BufferType::TEXTURE:
+        glBindBuffer(GL_ARRAY_BUFFER, buffers[type]);
+        glBufferData(GL_ARRAY_BUFFER,
+                     attribute->data_array_size,
+                     attribute->data,
+                     GL_STATIC_DRAW);
+
+        glEnableVertexAttribArray(type);
+        glVertexAttribPointer(type,
+                              attribute->data_size,
+                              attribute->data_type,
+                              GL_FALSE,
+                              0,
+                              0);
+        break;
+      case BufferType::ELEMENT:
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffers[type]);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER,
+                     attribute->data_array_size,
+                     attribute->data,
+                     GL_STATIC_DRAW);
+
+        element_type = attribute->data_type;
+        element_count = attribute->data_array_size / type_size;
+        break;
+      default:
+        break;
+    }
+  }
+
+  vao_buffers_.vertices[vao_name]  = buffers[BufferType::VERTEX];
+  vao_buffers_.normals[vao_name]   = buffers[BufferType::NORMAL];
+  vao_buffers_.texcoords[vao_name] = buffers[BufferType::TEXTURE];
+  
+  vao_buffers_.elements[vao_name]       = buffers[BufferType::ELEMENT];
+  vao_buffers_.element_types[vao_name]  = element_type;
+  vao_buffers_.element_counts[vao_name] = element_count;
+}
+
+void bar::MeshSystem::unload_buffers(uint32_t name_count, GLuint *vao_names) {
+  if (name_count == 0 || vao_names == nullptr) {
+    return;
+  }
+
+  GLuint buffers[BufferType::COUNT] = {0};
+  for (uint32_t i = 0; i < name_count; i++) {
+    GLuint name = vao_names[i];
+    buffers[BufferType::VERTEX]  = vao_buffers_.vertices[name];
+    buffers[BufferType::NORMAL]  = vao_buffers_.normals[name];
+    buffers[BufferType::TEXTURE] = vao_buffers_.texcoords[name];
+    buffers[BufferType::ELEMENT] = vao_buffers_.elements[name];
+    glDeleteBuffers(BufferType::COUNT, buffers);
+
+    vao_buffers_.vertices[name] = 0;
+    vao_buffers_.normals[name] = 0;
+    vao_buffers_.texcoords[name] = 0;
+    vao_buffers_.elements[name] = 0;
+    vao_buffers_.element_types[name] = 0;
+    vao_buffers_.element_counts[name] = 0;
+  }
+}
+
+void bar::MeshSystem::render_elements(uint32_t name_count, GLuint *vao_names) const {
+  if (name_count == 0 || vao_names == nulltpr) {
+    return;
+  }
+
+  GLenum *types = vao_buffers.element_types;
+  GLsizei *counts = vao_buffers.element_counts;
+
+  GLuint name = 0;
+  for (uint32_t i = 0; i < name_count; i++) {
+    name = vao_names[i];
+    glDrawElements(GL_TRIANGLES,
+                   types[name],
+                   counts[name],
+                   nullptr);
+  }
+}
---
+++ Renderer/MeshSystem.h
@@ -0,0 +1,77 @@
+// -*- mode: C++; -*-
+//
+//  MeshSystem.h
+//  Renderer
+//
+//  Created by Benjamin Anderson on 7/5/14.
+//  Copyright (c) 2014 Benjamin Anderson. All rights reserved.
+//
+
+#ifndef __Renderer__MeshSystem__
+#define __Renderer__MeshSystem__
+
+namespace bar {
+
+enum BufferType {
+  VERTEX = 0,
+  NORMAL,
+  TEXTURE,
+  ELEMENT,
+        
+  COUNT // MUST ALWAYS be the LAST value
+};
+
+struct MeshAttribute {
+  // The type of attribute
+  BufferType type;
+  // Mesh data
+  GLvoid *data;
+  // the data type of mesh data values
+  GLenum data_type;
+  // the number of components per attribute
+  GLuint data_size;
+  // the size in bytes of the mesh data array
+  GLsizei data_array_size;
+};
+
+// Buffer names for each mesh.
+// Indexed by vao_names;
+struct Buffers {
+  GLuint *vertices;
+  GLuint *normals;
+  GLuint *texcoords;
+  
+  GLuint *elements;        // The element buffer names
+  GLenum *element_types;   // The data type of elements at buffer name
+  GLsizei *element_counts; // The number of element at buffer name
+};
+
+class MeshSystem {
+ public:
+  explicit MeshSystem(uint32_t max_mesh_count);
+  virtual ~MeshSystem();
+
+  // Loads an array of meshes into buffers used by the renderer.
+  // vao_name - The name of the VAO to group the buffers under.
+  // attributes - The array of mesh attributes to load.
+  void load_buffers(GLuint vao_name, uint32_t attribute_count, MeshAttribute *attributes);
+
+  // Unloads the buffers associated with the given array of VAO names.
+  // name_count - The number of VAO names to unload.
+  // vao_names - The names of VAOs to unload buffers for.
+  void unload_buffers(uint32_t name_count, GLuint *vao_names);
+
+  // Renders the elements associated with the given array of VAO names.
+  // name_count - The number of VAO names to unload.
+  // vao_names - The names of VAOs to unload buffers for.
+  void render_elements(uint32_t name_count, GLuint *vao_names) const;
+  
+ private:
+  MeshSystem();
+
+  Buffers vao_buffers_;
+};
+
+}
+
+#endif /* defined(__Renderer__MeshSystem__) */
